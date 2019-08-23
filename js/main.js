f4015a19b6c68a0dab7adb9edadae981d47a3dfc
Nguyen Truong An <annguyen463@gmail.com>
2019-08-23 12:44:34 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -44,3 +44,8 @@ function step() {
   message = message.substr(1) + message.substr(0, 1);
   document.title = message.substr(0, 15);
 }
+
+window.onload = function(){
+	var audio = document.getElementsByTagName("audio");
+	audio[0].play();
+}
